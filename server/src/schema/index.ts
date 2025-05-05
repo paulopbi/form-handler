de3e56e9151b732fd56fe33a183e7fadf097d336
Paulo Victor Silva <paulpbi16@gmail.com>
2025-05-04 23:27:25 -0300

    feat: add zod schema

---
+++ server/src/schema/index.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+
+const values = {
+  maxZipCodeLength: 9,
+  maxCpfLength: 14,
+  maxStringLength: 255,
+  minPasswordValue: 8,
+  maxPasswordValue: 255,
+  maxPhoneLength: 20,
+};
+
+const customMessages = {
+  mandatory: "O campo é obrigatório",
+  minLengthPassword: `Precisa ter no mínimo ${values.minPasswordValue} caracteres`,
+  maxLengthPassword: `Pode ter no máximo ${values.maxPasswordValue} caracteres`,
+  maxLengthPhone: `Pode ter no máximo ${values.maxPhoneLength} números`,
+};
+
+export const DatabaseSchema = z.object({
+  email: z.string().email().nonempty(customMessages.mandatory),
+  cpf: z.string().max(values.maxCpfLength).nonempty(customMessages.mandatory),
+  state: z.string().nonempty(customMessages.mandatory),
+  city: z.string().nonempty(customMessages.mandatory),
+  name: z
+    .string()
+    .max(values.maxStringLength)
+    .nonempty(customMessages.mandatory),
+  zip_code: z
+    .string()
+    .max(values.maxZipCodeLength)
+    .nonempty(customMessages.mandatory),
+  phone: z
+    .string()
+    .max(values.maxPhoneLength, customMessages.maxLengthPhone)
+    .nonempty(customMessages.mandatory),
+  password: z
+    .string()
+    .min(values.minPasswordValue, customMessages.minLengthPassword)
+    .max(values.maxPasswordValue, customMessages.maxLengthPassword),
+  terms: z.literal(true),
+});
+
+export type DatabaseSchemaType = z.infer<typeof DatabaseSchema>;
