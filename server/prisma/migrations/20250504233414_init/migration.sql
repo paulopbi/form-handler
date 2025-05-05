eecc811a26c83206301f2dc7dd6e5de95dac11ba
Paulo Victor Silva <paulpbi16@gmail.com>
2025-05-04 23:30:25 -0300

    feat: add sqlite database

---
+++ server/prisma/migrations/20250504233414_init/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "zip_code" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "terms" BOOLEAN NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_cpf_key" ON "User"("cpf");
