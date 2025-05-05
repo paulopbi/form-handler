a75fe2ceb0480c8a9ca6265ff45c717e51e6709c
Paulo Victor Silva <paulpbi16@gmail.com>
2025-05-04 23:29:21 -0300

    feat: add types

---
+++ client/src/types/index.ts
@@ -0,0 +1,15 @@
+export type ZipCodeType = {
+  bairro: string;
+  cep: string;
+  complemento: string;
+  ddd: string;
+  estado: string;
+  gia: string;
+  ibge: string;
+  localidade: string;
+  logradouro: string;
+  regiao: string;
+  siafi: string;
+  uf: string;
+  unidade: string;
+};
